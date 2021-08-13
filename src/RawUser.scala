2445b461285821e7b9b1f1de272619c0deab1327
Alexandr Chudopal <alex.ch@core-tech.ru>
2021-08-13 17:15:48 +0300

    added parser in RawUser

--- src/RawUser.scala
+++ src/RawUser.scala
@@ -1,9 +1,14 @@
 case class RawUser(
-    fullName: String,
-    email: String,
-    phone: String,
-    streetAddress: String,
-    city: String,
-    zipCode: String) {
-  lazy val person: Person = ???
+                    fullName: String,
+                    email: String,
+                    phone: String,
+                    streetAddress: String,
+                    city: String,
+                    zipCode: String) {
+  lazy val person: Either[TransformError, Person] = {
+    fullName.split(" ").toList match {
+      case first :: last :: Nil => Right(Person(first, last))
+      case _ => Left(TransformError(s"Failing parsing person"))
+    }
+  }
 }
